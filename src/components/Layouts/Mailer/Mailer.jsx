cd8efa9fe3e74a1b1c89fdad9f646d2a18d53ea6
jefferson izquierdo <jeffersondavid803@gmail.com>
2022-06-02 10:59:16 -0500

    envio de correos

--- src/components/Layouts/Mailer/Mailer.jsx
+++ src/components/Layouts/Mailer/Mailer.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
+import emailjs from '@emailjs/browser';
 
 export const Mailer = () => {
+
+  const sendEmail = (event) => {
+
+    event.preventDefault();
+
+    emailjs.sendForm('service_270jbap', 'template_60hszbu', event.target, 'vraW1vtbk2fJX_aSe')
+    .then(response => alert('Correo enviado', response))
+    .catch(erorr => alert('Erro al enviar', erorr))
+
+  }
 
   return (
     <div className='form_div'>
      
-     <h1> Contacts US </h1>
-      <form  className='form'>
-        <label htmlFor="">Name</label>
-        <input type="text" name='userName' />
-        <hr></hr>
+     <h2> Contacts US </h2>
+      <form  onSubmit={sendEmail} className='form'>
+        <nav className='containerInput' >
+          <label htmlFor="">Name</label>
+          <input className='input1' type="text" name='userName' />
+        </nav>
+        
+        <nav className='containerInput' >
+          <label htmlFor="">Email</label>
+          <input className='input1' type="text" name='userEmail' />  
+        </nav>
+        <nav className='containerInput' >
+          <label htmlFor="">Meassage</label>
+          <textarea className='inputArea' type="text" name='userMessage' id='' cols="30" rows="10" />
+        </nav>
 
-        <label htmlFor="">Email</label>
-        <input type="text" name='userEmail' />
-        <hr></hr>
+        
 
-        <label htmlFor="">Meassage</label>
-        <textarea type="text" name='userMessage' id='' cols="30" rows="10" />
-        <hr></hr>
 
-        <button>Send</button>
+        <button className='btnSend' >Enviar</button>
       </form>
     </div>
   )
