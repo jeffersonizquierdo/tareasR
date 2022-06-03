1a7897dccfad6c5057a08827cff63609f28952ba
jefferson izquierdo <jeffersondavid803@gmail.com>
2022-06-03 15:57:52 -0500

    correccion de error en el consumo de la api

--- src/components/Layouts/ApiTenor/ApiTenor.jsx
+++ src/components/Layouts/ApiTenor/ApiTenor.jsx
@@ -1,30 +1,32 @@
 import React, { useEffect, useState } from 'react'
+import { Input } from '../../Ui/Input/Input'
 
 
 export const ApiTenor = () => {
 
+    const inputShearch = document.getElementById('inputGif')
     const container = document.getElementById('container')
 
     const [mode, setMode] = useState([])
 
-   
+    
 
-    const custonmGif = (event) =>{
+    const custonmGif = (event, inputShearch) =>{
 
         const container = document.getElementById('container')
         container.innerText = ""
 
-        const URL = "https://g.tenor.com/v1/search?";
+        const searchInput = inputShearch
+        const URL = "https://g.tenor.com/v1/search?"+inputShearch;
         const key = "QQO4FEBQNWYA";
         const query = `q=${event.target.value}`;
-        const limit = "&limit=10";  
+        const limit = "&limit=20";  
 
         fetch(`${URL}${query}&key=${key}${limit}`)
         .then(response => response.json())
-        .then(data => {
-            setMode(data) 
-            createGif(data)
-        console.log(data);
+        .then(data => { createGif(data)
+            setMode(data)
+            console.log(data);
         })
     }
     window.addEventListener('load', custonmGif)
@@ -41,9 +43,10 @@ export const ApiTenor = () => {
     }
   return (
     <div>
-        <main id='container'>
 
+        <Input id={'inputGif'} event={custonmGif} placeh={'Buscar'} />
 
+        <main className='container' id='container'>
 
         </main>
     </div>
